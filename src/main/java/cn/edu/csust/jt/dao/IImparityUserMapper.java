b68848845fb43ced16e88da8ad2c48728e464e52
MeiYuJijieYihou <184606905@qq.com>
2020-01-27 21:24:19 +0800

    新增实体类中属性名与数据库中字段名不相同功能的测试类以及配置文件的修改。

---
+++ src/main/java/cn/edu/csust/jt/dao/IImparityUserMapper.java
@@ -0,0 +1,69 @@
+package cn.edu.csust.jt.dao;
+
+import cn.edu.csust.jt.entity.QueryVo;
+import cn.edu.csust.jt.entity.User;
+
+import java.util.List;
+
+/**
+ * @Institution csust
+ * @Author MeiyuJijieYihou
+ * @Description Waiting fo development.
+ * @Date 2020/1/27 下午9:14
+ */
+public interface IImparityUserMapper {
+
+    /**
+     * 查询所有用户信息的操作
+     * @return
+     */
+    List<User> findAll();
+
+
+    /**
+     * 保存用户数据
+     */
+    void saveUser(User user);
+
+    /**
+     * 更新操作
+     * @param user
+     */
+    void updateUser(User user);
+
+
+    /**
+     * 删除操作
+     * @param uid
+     */
+    void deleteUser(Integer uid);
+
+
+    /**
+     * 根据id查询用户信息
+     * @param uid
+     */
+    User findById(Integer uid);
+
+
+    /**
+     * 根据用户姓名查询用户信息
+     * @param name
+     * @return
+     */
+    List<User> findByName(String name);
+
+
+    /**
+     * 查找总记录条数
+     * @return
+     */
+    Integer findTotal();
+
+
+    /**
+     * 通过Vo对象查询数据
+     * @return
+     */
+    List<User> findByUserVo(QueryVo queryVo);
+}
---
+++ src/main/java/cn/edu/csust/jt/entity/ImparityUser.java
@@ -0,0 +1,10 @@
+package cn.edu.csust.jt.entity;
+
+/**
+ * @Institution csust
+ * @Author MeiyuJijieYihou
+ * @Description Waiting fo development.
+ * @Date 2020/1/27 下午9:15
+ */
+public class ImparityUser {
+}
